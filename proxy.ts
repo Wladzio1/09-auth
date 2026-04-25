6bcdcb5ab3c999e2eaeb43f1a08a6237179dcf74
Wladek <vladislavfityak@gmail.com>
2026-04-25 18:30:38 +0200

    fxed proxy file

--- proxy.ts
+++ proxy.ts
@@ -1,19 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import { checkSession } from "@/lib/api/serverApi";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  const isAuthPage =
+  const isAuthRoute =
     pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up");
-  const isPrivate =
+
+  const isPrivateRoute =
     pathname.startsWith("/profile") || pathname.startsWith("/notes");
 
   let response = NextResponse.next();
 
+  // refresh session
   if (!accessToken && refreshToken) {
     try {
       const res = await checkSession();
@@ -28,17 +30,19 @@ export async function middleware(request: NextRequest) {
         });
       }
     } catch {
-      if (isPrivate) {
+      if (isPrivateRoute) {
         return NextResponse.redirect(new URL("/sign-in", request.url));
       }
     }
   }
 
-  if (!accessToken && isPrivate) {
+  // no access
+  if (!accessToken && isPrivateRoute) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
-  if (accessToken && isAuthPage) {
+  // logged in users on auth pages
+  if (accessToken && isAuthRoute) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
