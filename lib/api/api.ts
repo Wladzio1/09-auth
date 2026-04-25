ed022b4417fde4f81ec06ad92e1532cbdc7a9b37
Wladek <vladislavfityak@gmail.com>
2026-04-25 17:09:26 +0200

    fixed the api url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL ?? "https://notehub-api.goit.study";
 
 export const api = axios.create({
   baseURL,
